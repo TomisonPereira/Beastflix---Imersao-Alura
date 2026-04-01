fb4899e4273bf056bacb542b3ad7d67f7a8b0719
Tomison Pereira de Freitas <tomison07@gmail.com>
2026-03-31 22:57:38 -0300

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,7 @@
+alert("Bem-vindo ao Beastflix!");
+console.log("Bem-vindo ao Beastflix!");
+
+// Função para exibir um alerta de boas-vindas
+function mostrarBoasVindas() {
+    alert("Bem-vindo ao Beastflix!");
+}
